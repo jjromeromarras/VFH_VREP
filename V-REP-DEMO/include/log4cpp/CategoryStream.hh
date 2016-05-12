de155c43dc1036a107f1ebc600f60c90e851578f
jjromeromarras <jjromeromarras@gmail.com>
2016-05-12 22:39:27 +0200

    Add files via upload

---
+++ V-REP-DEMO/include/log4cpp/CategoryStream.hh
@@ -0,0 +1,104 @@
+/*
+ * CategoryStream.hh
+ *
+ * Copyright 2001, LifeLine Networks BV (www.lifeline.nl). All rights reserved.
+ * Copyright 2001, Bastiaan Bakker. All rights reserved.
+ *
+ * See the COPYING file for the terms of usage and distribution.
+ */
+
+#ifndef _LOG4CPP_CATEGORYSTREAM_HH
+#define _LOG4CPP_CATEGORYSTREAM_HH
+
+#include <log4cpp/Portability.hh>
+#include <log4cpp/Priority.hh>
+#ifdef LOG4CPP_HAVE_SSTREAM
+#include <sstream>
+#endif
+
+namespace log4cpp {
+
+    class LOG4CPP_EXPORT Category;
+
+    /**
+     * This class enables streaming simple types and objects to a category.
+     * Use category.errorStream(), etc. to obtain a CategoryStream class.
+     **/
+    class LOG4CPP_EXPORT CategoryStream {
+        public:
+
+        /**
+         * Enumeration of special 'Separators'. Currently only contains the
+         * 'ENDLINE' separator, which separates two log messages.
+         **/
+        typedef enum {
+            ENDLINE
+        } Separator;
+
+        /**
+         * Construct a CategoryStream for given Category with given priority.
+         * @param category The category this stream will send log messages to.
+         * @param priority The priority the log messages will get or 
+         * Priority::NOTSET to silently discard any streamed in messages.
+         **/
+        CategoryStream(Category& category, Priority::Value priority);
+
+        /**
+         * Destructor for CategoryStream
+         **/
+        ~CategoryStream();
+        
+        /**
+         * Returns the destination Category for this stream.
+         * @returns The Category.
+         **/
+        inline Category& getCategory() const { return _category; };
+
+        /**
+         * Returns the priority for this stream.
+         * @returns The priority.
+         **/
+        inline Priority::Value getPriority() const throw() { 
+            return _priority; 
+        };
+
+        /**
+         * Streams in a Separator. If the separator equals 
+         * CategoryStream::ENDLINE it sends the contents of the stream buffer
+         * to the Category with set priority and empties the buffer.
+         * @param separator The Separator
+         * @returns A reference to itself.
+         **/
+        CategoryStream& operator<<(Separator separator);
+
+        /**
+         * Flush the contents of the stream buffer to the Category and
+         * empties the buffer.
+         **/
+        void flush();
+
+        /**
+         * Stream in arbitrary types and objects.  
+         * @param t The value or object to stream in.
+         * @returns A reference to itself.
+         **/
+        template<typename T> CategoryStream& operator<<(const T& t) {
+            if (getPriority() != Priority::NOTSET) {
+                if (!_buffer) {
+                    if (!(_buffer = new std::ostringstream)) {
+                        // XXX help help help
+                    }
+                }
+                (*_buffer) << t;
+            }
+            return *this;
+        }
+        
+        private:
+        Category& _category;
+        Priority::Value _priority;
+        std::ostringstream* _buffer;
+    };
+
+}
+#endif // _LOG4CPP_CATEGORYSTREAM_HH
---
+++ V-REP-DEMO/include/log4cpp/Portability.hh
@@ -0,0 +1,41 @@
+/*
+ * Portability.hh
+ *
+ * Copyright 2001, LifeLine Networks BV (www.lifeline.nl). All rights reserved.
+ * Copyright 2001, Bastiaan Bakker. All rights reserved.
+ *
+ * See the COPYING file for the terms of usage and distribution.
+ */
+
+#ifndef _LOG4CPP_PORTABILITY_HH
+#define _LOG4CPP_PORTABILITY_HH
+
+#if defined (_MSC_VER) || defined(__BORLANDC__)
+#    include <log4cpp/config-win32.h>
+#else
+#if defined(__OPENVMS__)
+#    include <log4cpp/config-openvms.h>
+#else
+#    include <log4cpp/config.h>
+#endif
+#endif
+
+#include <log4cpp/Export.hh>
+
+#if defined(_MSC_VER)
+#    pragma warning( disable : 4786 ) // 255 char debug symbol limit
+#    pragma warning( disable : 4290 ) // throw specifier not implemented
+#    pragma warning( disable : 4251 ) // "class XXX should be exported"
+#endif
+
+#ifndef LOG4CPP_HAVE_SSTREAM
+#include <strstream>
+namespace std {
+    class LOG4CPP_EXPORT ostringstream : public ostrstream {
+        public:
+        std::string str();
+    };
+}
+#endif
+
+#endif
